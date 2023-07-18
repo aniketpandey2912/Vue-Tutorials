aa40cad9eed1ba7f7d6cdf03c8ae812e7f4e258d
aniketpandey2912 <aniketpandey2912@gmail.com>
2023-07-18 21:54:13 +0530

    added counter

---
+++ vue-store-1/src/components/Counter.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Counter : {{ count }}</h1>
+    <div>
+      <button @click="decrement()">DEC</button>
+      <button @click="increment()">INC</button>
+      <button @click="reset()">RESET</button>
+    </div>
+  </div>
+</template>
+
+<!-- This way of writing script is COMPOSITION API - Modern way -->
+<!-- <script setup>
+// to make variables reactive, we need "ref"
+import { ref } from "vue";
+
+let count = ref(0);
+console.log(count);
+
+const increment = () => {
+  count.value++;
+  console.log(count);
+};
+
+const decrement = () => {
+  count.value--;
+  console.log(count);
+};
+
+const reset = () => {
+  count.value = 0;
+  console.log(count);
+};
+</script> -->
+
+
+
+<style scoped>
+button {
+  cursor: pointer;
+}
+</style>
