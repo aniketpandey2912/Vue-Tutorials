e8d82f3f37579b9db21b40ae78a837aa0df71610
aniketpandey2912 <aniketpandey2912@gmail.com>
2023-07-19 22:22:41 +0530

    added banner

--- vue-store-2/e-store/src/App.vue
+++ vue-store-2/e-store/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <NavbarComponent />
+    <BannerComponent welcome_msg="Welcome To Vue Shop" />
   </div>
 </template>
 
 <script setup>
 import NavbarComponent from "./components/Navbar.vue";
+import BannerComponent from "./components/Banner.vue";
 </script>
 
 <style></style>
